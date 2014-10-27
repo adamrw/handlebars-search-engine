0fd3585d0cb5006a2bd971cf1f4161d995d54307
Adam Weiner <adamwineguy@gmail.com>
2014-10-27 17:14:29 -0600

    beginning work

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
+app.get('/views/search', indexController.search);
 
-var server = app.listen(8116, function() {
+var port = process.env.PORT || 116; 
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
---
+++ controllers/apiController.js
@@ -0,0 +1,6 @@
+var apiController = {
+
+
+};
+
+module.exports = apiController;
