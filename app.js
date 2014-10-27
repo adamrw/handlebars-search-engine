8242d9175de63f1174ddb065ac83365408c13c3c
Adam Weiner <adamwineguy@gmail.com>
2014-10-27 17:49:20 -0600

    adding search functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
+var search = require('./controllers/search-data.js');
 
 var app = express();
 app.set('view engine', 'jade');
@@ -9,9 +10,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
-app.get('/views/search', indexController.search);
+app.get('/search', indexController.search);
 
-var port = process.env.PORT || 116; 
+var port = process.env.PORT || 1169; 
 var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
